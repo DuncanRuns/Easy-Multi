8bc4005b50c13867a9b8d24ba2b732c6e7302c06
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2022-04-09 14:49:07 -0300

    Reformat

--- hwnd_util.py
+++ hwnd_util.py
@@ -34,7 +34,7 @@ def get_hwnd_title(hwnd: int) -> str:
 
 
 def _win_enum_handler(hwnd: int, hwnd_list: list):
-    hwnd_list.insert(0,hwnd)
+    hwnd_list.insert(0, hwnd)
 
 
 def get_all_hwnds() -> list:
@@ -51,10 +51,12 @@ def get_all_mc_hwnds(old_hwnds=[]) -> list:
             hwnds.append(hwnd)
     return hwnds
 
+
 def get_hwnd_exists(hwnd: int) -> bool:
     if win32gui.IsWindow(hwnd):
         return True
     return False
+
 
 def get_hwnd_style(hwnd: int) -> int:
     return win32gui.GetWindowLong(hwnd, GWL_STYLE)
@@ -77,13 +79,13 @@ def set_hwnd_borderless(hwnd: int, window_size=(1920, 1080)) -> None:
     win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, 0, 0,
                           window_size[0], window_size[1], win32con.SWP_SHOWWINDOW)
 
+
 def is_hwnd_borderless(hwnd: int) -> bool:
     old_style = get_hwnd_style(hwnd)
     new_style = old_style
     new_style &= ~(WS_BORDER | WS_DLGFRAME | WS_THICKFRAME |
-               WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU)
+                   WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU)
     return new_style == old_style
-    
 
 
 def undo_hwnd_borderless(hwnd, offset=0) -> None:
