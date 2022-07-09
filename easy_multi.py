1c6942608c7531ab7ee4a18238e0ac01c8a7dc40
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2022-07-09 19:50:56 -0300

    Update easy_multi.py

--- easy_multi.py
+++ easy_multi.py
@@ -1,6 +1,13 @@
+import threading
 from minecraft_instance import MinecraftInstance, get_all_mc_instances
+from easy_multi_options import EasyMultiOptions
 from key_util import *
 
 
 class EasyMulti:
-    pass
+    def __init__(self, options: EasyMultiOptions) -> None:
+        self._tick_lock = threading.Lock()
+
+    def tick(self) -> None:
+        with self._tick_lock:
+            pass
